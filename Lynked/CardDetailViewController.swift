d67fec39f8f180e706abfbe855cfa10c755a3cd9
mattmega4 <matthewhsingleton@matthews-macbook-pro.local>
2017-05-15 11:58:13 -0700

    s

---
+++ Lynked/CardDetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  CardDetailViewController.swift
+//  Lynked
+//
+//  Created by Matthew Howes Singleton on 5/14/17.
+//  Copyright © 2017 Matthew Singleton. All rights reserved.
+//
+
+import UIKit
+
+class CardDetailViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    
+    
+
+}
+
+//extension LandingPageViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+//    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        
+//        let padding: CGFloat = 25
+//        let collectionCellSize = collectionView.frame.size.width - padding
+//        
+//        return CGSize(width: collectionCellSize/2, height: collectionCellSize/2)
+//        
+//    }
+//    
+//}
---
+++ Lynked/ServiceCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ServiceCollectionViewCell.swift
+//  Lynked
+//
+//  Created by Matthew Howes Singleton on 5/14/17.
+//  Copyright © 2017 Matthew Singleton. All rights reserved.
+//
+
+import UIKit
+
+class ServiceCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var colorStatusView: UIView!
+    @IBOutlet weak var whiteBackgroundView: UIView!
+    @IBOutlet weak var serviceNameLabel: UILabel!
+    @IBOutlet weak var serviceLogoImage: UIImageView!
+    
+}
