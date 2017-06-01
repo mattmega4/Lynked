9ccd78bbe4270ba26d5a923cc9b363aeec236561
mattmega4 <matthewhsingleton@matthews-macbook-pro.local>
2017-05-31 18:55:49 -0700

    adding old code back in

--- Lynked/AddCardViewController.swift
+++ Lynked/AddCardViewController.swift
@@ -11,179 +11,177 @@ import Firebase
 import FirebaseDatabase
 
 class AddCardViewController: UIViewController {
-  
-  @IBOutlet weak var cancelNavBarButton: UIBarButtonItem!
-  @IBOutlet weak var nextNavBarButton: UIBarButtonItem!
-  @IBOutlet weak var scrollView: UIScrollView!
-  @IBOutlet weak var contentView: UIView!
-  @IBOutlet weak var mainImageView: UIImageView!
-  @IBOutlet weak var labelUnderMainImage: UILabel!
-  @IBOutlet weak var authorLabel: UILabel!
-  @IBOutlet weak var firstDividerView: UIView!
-  @IBOutlet weak var secondDividerView: UIView!
-  @IBOutlet weak var thirdDividerView: UIView!
-  @IBOutlet weak var firstContainerView: UIView!
-  @IBOutlet weak var secondContainerView: UIView!
-  @IBOutlet weak var firstContainerLabel: UILabel!
-  @IBOutlet weak var secondContainerLabel: UILabel!
-  @IBOutlet weak var firstContainerTextField: UITextField!
-  @IBOutlet weak var secondContainerTextField: UITextField!
-  @IBOutlet weak var finalNoteLabel: UILabel!
-  
-  let ref = Database.database().reference()
-  var nickNameTextFieldIsEmpty = true
-  var cardTypeTextFieldIsEmpty = true
-  var finalNickname: String?
-  var finalType: String?
-  
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
     
-    self.firstContainerTextField.delegate = self
-    self.secondContainerTextField.delegate = self
-    setNavBar()
-    nextNavBarButton.isEnabled = false
-    firstContainerTextField.addTarget(self, action: #selector(checkNicknameTextField(textField:)), for: .editingChanged)
-    secondContainerTextField.addTarget(self, action: #selector(checkTypeTextField(textField:)), for: .editingChanged)
-    let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(AddCardViewController.dismissKeyboard))
-    view.addGestureRecognizer(tap)
-    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name:NSNotification.Name.UIKeyboardWillShow, object: nil)
-    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name:NSNotification.Name.UIKeyboardWillHide, object: nil)
+    @IBOutlet weak var cancelNavBarButton: UIBarButtonItem!
+    @IBOutlet weak var nextNavBarButton: UIBarButtonItem!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var firstDividerView: UIView!
+    @IBOutlet weak var secondDividerView: UIView!
+    @IBOutlet weak var thirdDividerView: UIView!
+    @IBOutlet weak var firstContainerView: UIView!
+    @IBOutlet weak var secondContainerView: UIView!
+    @IBOutlet weak var firstContainerLabel: UILabel!
+    @IBOutlet weak var secondContainerLabel: UILabel!
+    @IBOutlet weak var firstContainerTextField: UITextField!
+    @IBOutlet weak var secondContainerTextField: UITextField!
+    @IBOutlet weak var finalNoteLabel: UILabel!
     
-  }
-  
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
+    let ref = Database.database().reference()
+    var nickNameTextFieldIsEmpty = true
+    var cardTypeTextFieldIsEmpty = true
+    var finalNickname: String?
+    var finalType: String?
     
-    checkIfAllConditionsAreMet()
-  }
-  
-  
-  // MARK: Nav Bar & View Design
-  
-  func setNavBar() {
-    title = "Add Card"
-    navigationController?.navigationBar.barTintColor = UIColor(red: 108.0/255.0,
-                                                               green: 158.0/255.0,
-                                                               blue: 236.0/255.0,
-                                                               alpha: 1.0)
-    UINavigationBar.appearance().tintColor = .white
-    UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName : UIColor.white]
-    navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white,
-                                                               NSFontAttributeName: UIFont(name: "GillSans-Bold",
-                                                                                           size: 20)!]
-  }
-  
-  
-  // MARK: Write to Firebase
-  
-  func addDataToFirebase() {
-    let nicknameToAdd = firstContainerTextField.text ?? ""
-    let nicknameWithoutWhiteSpaces = nicknameToAdd.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-    let typeToAdd = secondContainerTextField.text ?? ""
-    let typeWithoutWhiteSpaces = typeToAdd
-    finalNickname = nicknameWithoutWhiteSpaces.capitalized
-    finalType = typeWithoutWhiteSpaces.capitalized
-    let user = Auth.auth().currentUser
-    let card = ref.child("cards").childByAutoId()
-    if let tempNick = finalNickname, let tempType = finalType {
-      card.setValue(["nickname": tempNick, "type": tempType, "cardStatus": true])
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.firstContainerTextField.delegate = self
+        self.secondContainerTextField.delegate = self
+        setNavBar()
+        nextNavBarButton.isEnabled = false
+        firstContainerTextField.addTarget(self, action: #selector(checkNicknameTextField(textField:)), for: .editingChanged)
+        secondContainerTextField.addTarget(self, action: #selector(checkTypeTextField(textField:)), for: .editingChanged)
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(AddCardViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name:NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name:NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
     }
-    ref.child("users").child((user?.uid)!).child("cards").child(card.key).setValue(true)
-    performSegue(withIdentifier: "fromAddCardToLandingPage", sender: self)
-  }
-  
-  
-  // MARK: Enable Next Button
-  
-  func checkIfAllConditionsAreMet() {
-    if nickNameTextFieldIsEmpty == false && cardTypeTextFieldIsEmpty == false {
-      nextNavBarButton.isEnabled = true
-    } else {
-      nextNavBarButton.isEnabled = false
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        checkIfAllConditionsAreMet()
     }
-  }
-  
-  
-  // MARK: IB Actions
-  
-  @IBAction func navBarCancelButtonTapped(_ sender: UIBarButtonItem) {
-    performSegue(withIdentifier: "fromAddCardToLandingPage", sender: self)
-  }
-  
-  @IBAction func navBarNextButtonTapped(_ sender: UIBarButtonItem) {
-    addDataToFirebase()
-  }
-  
-
-  // MARK: Keyboard Methods
-  
-  func keyboardWillShow(notification:NSNotification) {
-    var userInfo = notification.userInfo!
-    var keyboardFrame:CGRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
-    keyboardFrame = self.view.convert(keyboardFrame, from: nil)
-    var contentInset: UIEdgeInsets = self.scrollView.contentInset
-    contentInset.bottom = keyboardFrame.size.height + 30
-    self.scrollView.contentInset = contentInset
-  }
-  
-  
-  func keyboardWillHide(notification:NSNotification) {
-    let contentInset:UIEdgeInsets = UIEdgeInsets.zero
-    self.scrollView.contentInset = contentInset
-  }
-  
-  
-  func dismissKeyboard() {
-    view.endEditing(true)
-  }
-  
-  
-  func textFieldShouldClear(_ textField: UITextField) -> Bool {
-    self.view.endEditing(true)
-    return true
-  }
-  
-  
-  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-    self.view.endEditing(true)
-    textField.resignFirstResponder()
-    return false
-  }
-  
-  
+    
+    
+    // MARK: Nav Bar & View Design
+    
+    func setNavBar() {
+        title = "Add Card"
+        navigationController?.navigationBar.barTintColor = UIColor(red: 108.0/255.0,
+                                                                   green: 158.0/255.0,
+                                                                   blue: 236.0/255.0,
+                                                                   alpha: 1.0)
+        UINavigationBar.appearance().tintColor = .white
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName : UIColor.white]
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white,
+                                                                   NSFontAttributeName: UIFont(name: "GillSans-Bold",
+                                                                                               size: 20)!]
+    }
+    
+    
+    // MARK: Write to Firebase
+    
+    func addDataToFirebase() {
+        let nicknameToAdd = firstContainerTextField.text ?? ""
+        let nicknameWithoutWhiteSpaces = nicknameToAdd.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let typeToAdd = secondContainerTextField.text ?? ""
+        let typeWithoutWhiteSpaces = typeToAdd
+        finalNickname = nicknameWithoutWhiteSpaces.capitalized
+        finalType = typeWithoutWhiteSpaces.capitalized
+        let user = Auth.auth().currentUser
+        let card = ref.child("cards").childByAutoId()
+        if let tempNick = finalNickname, let tempType = finalType {
+            card.setValue(["nickname": tempNick, "type": tempType, "cardStatus": true])
+        }
+        ref.child("users").child((user?.uid)!).child("cards").child(card.key).setValue(true)
+        performSegue(withIdentifier: "fromAddCardToCardDetails", sender: self)
+    }
+    
+    
+    // MARK: Enable Next Button
+    
+    func checkIfAllConditionsAreMet() {
+        if nickNameTextFieldIsEmpty == false && cardTypeTextFieldIsEmpty == false {
+            nextNavBarButton.isEnabled = true
+        } else {
+            nextNavBarButton.isEnabled = false
+        }
+    }
+    
+    
+    // MARK: IB Actions
+    
+    @IBAction func navBarCancelButtonTapped(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "fromAddCardToCardDetails", sender: self)
+    }
+    
+    @IBAction func navBarNextButtonTapped(_ sender: UIBarButtonItem) {
+        addDataToFirebase()
+    }
+    
+    
+    // MARK: Keyboard Methods
+    
+    func keyboardWillShow(notification:NSNotification) {
+        var userInfo = notification.userInfo!
+        var keyboardFrame:CGRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        keyboardFrame = self.view.convert(keyboardFrame, from: nil)
+        var contentInset: UIEdgeInsets = self.scrollView.contentInset
+        contentInset.bottom = keyboardFrame.size.height + 30
+        self.scrollView.contentInset = contentInset
+    }
+    
+    
+    func keyboardWillHide(notification:NSNotification) {
+        let contentInset:UIEdgeInsets = UIEdgeInsets.zero
+        self.scrollView.contentInset = contentInset
+    }
+    
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    
 } // End of AddCardViewController Class
 
 
 // MARK: UITextField Methods
 
 extension AddCardViewController: UITextFieldDelegate {
-  
-  func checkNicknameTextField(textField: UITextField) {
-    if textField == firstContainerTextField {
-      if textField.text?.isEmpty == true {
-        nickNameTextFieldIsEmpty = true
-      } else {
-        nickNameTextFieldIsEmpty = false
-      }
+    
+    func checkNicknameTextField(textField: UITextField) {
+        if textField == firstContainerTextField {
+            if textField.text?.isEmpty == true {
+                nickNameTextFieldIsEmpty = true
+            } else {
+                nickNameTextFieldIsEmpty = false
+            }
+        }
+        checkIfAllConditionsAreMet()
     }
-    checkIfAllConditionsAreMet()
-  }
-  
-  
-  func checkTypeTextField(textField: UITextField) {
-    if textField == secondContainerTextField {
-      if textField.text?.isEmpty == true {
-        cardTypeTextFieldIsEmpty = true
-      } else {
-        cardTypeTextFieldIsEmpty = false
-      }
+    
+    
+    func checkTypeTextField(textField: UITextField) {
+        if textField == secondContainerTextField {
+            if textField.text?.isEmpty == true {
+                cardTypeTextFieldIsEmpty = true
+            } else {
+                cardTypeTextFieldIsEmpty = false
+            }
+        }
+        checkIfAllConditionsAreMet()
     }
-    checkIfAllConditionsAreMet()
-  }
-  
-  
+    
+    
 }
 
 
---
+++ Lynked/WalletViewController.swift
@@ -0,0 +1,184 @@
+//
+//  WalletViewController.swift
+//  Lynked
+//
+//  Created by Matthew Howes Singleton on 5/31/17.
+//  Copyright © 2017 Matthew Singleton. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+
+class WalletViewController: UIViewController {
+    
+    
+    @IBOutlet weak var leftNavButton: UIBarButtonItem!
+    @IBOutlet weak var rightNavButton: UIBarButtonItem!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var selectedCard: String?
+    var cardNicknameToTransfer = ""
+    var cardtypeToTransfer = ""
+    let ref = Database.database().reference()
+    let user = Auth.auth().currentUser
+    var cardArray: [CardClass] = []
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        setNavBar()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        checkIfDataExits()
+        tableView.isUserInteractionEnabled = true
+        
+    }
+    
+    
+    
+    // MARK: Nav Bar & View Design
+    
+    func setNavBar() {
+        title = "Wallet"
+        navigationController?.navigationBar.barTintColor = UIColor(red: 108.0/255.0,
+                                                                   green: 158.0/255.0,
+                                                                   blue: 236.0/255.0,
+                                                                   alpha: 1.0)
+        UINavigationBar.appearance().tintColor = .white
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName : UIColor.white]
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white,
+                                                                   NSFontAttributeName: UIFont(name: "GillSans-Bold",
+                                                                                               size: 20)!]
+    }
+    
+    
+    
+    
+    // MARK: Firebase Methods
+    
+    func checkIfDataExits() {
+        DispatchQueue.main.async {
+            self.cardArray.removeAll()
+            self.ref.observeSingleEvent(of: .value, with: { snapshot in
+                if snapshot.hasChild("cards") {
+                    self.pullAllUsersCards()
+                } else {
+                    self.tableView.reloadData()
+                }
+            })
+        }
+    }
+    
+    
+    func pullAllUsersCards() {
+        cardArray.removeAll()
+        let userRef = ref.child("users").child((user?.uid)!).child("cards")
+        userRef.observeSingleEvent(of: .value, with: { snapshot in
+            for userscard in snapshot.children {
+                let cardID = (userscard as AnyObject).key as String
+                let cardRef = self.ref.child("cards").child(cardID)
+                cardRef.observeSingleEvent(of: .value, with: { cardSnapShot in
+                    let cardSnap = cardSnapShot as DataSnapshot
+                    let cardDict = cardSnap.value as! [String: AnyObject]
+                    let cardNickname = cardDict["nickname"]
+                    let cardType = cardDict["type"]
+                    let cardStatus = cardDict["cardStatus"]
+                    self.cardNicknameToTransfer = cardNickname as! String
+                    self.cardtypeToTransfer = cardType as! String
+                    let aCard = CardClass()
+                    aCard.cardID = cardID
+                    aCard.nickname = cardNickname as! String
+                    aCard.type = cardType as! String
+                    aCard.cStatus = cardStatus as! Bool
+                    self.cardArray.append(aCard)
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                })
+            }
+        })
+    }
+    
+    
+    
+    // MARK: Prepare for Segue Methods
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "fromLandingPageToCardDetails" {
+            if let controller = segue.destination as? UINavigationController {
+                if let destinationVC = controller.topViewController as? CardDetailsViewController {
+                    destinationVC.cardID = selectedCard!
+                }
+            }
+        }
+    }
+    
+    
+    // MARK: IB Actions
+    
+//    @IBAction func leftBarButtonTapped(_ sender: UIBarButtonItem) {
+//        performSegue(withIdentifier: "fromLandingPageToPref", sender: self)
+//    }
+//    
+//    @IBAction func rightBarButtonTapped(_ sender: UIBarButtonItem) {
+//        performSegue(withIdentifier: "fromLandingPageToAddCard", sender: self)
+//    }
+    
+    
+}
+
+
+// MARK: TableView Data Source Methods
+
+extension WalletViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath as IndexPath) as! CardTableViewCell
+        let row = indexPath.row
+        cell.cardNicknameLabel.text = cardArray[row].nickname
+        cell.cardTypeLabel.text = cardArray[row].type
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cardArray.count
+    }
+    
+    
+}
+
+
+// MARK: TableView Delegate Methods
+
+extension WalletViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 125.0
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        DispatchQueue.main.async {
+            let row = indexPath.row
+            self.selectedCard = self.cardArray[row].cardID as String
+            if self.selectedCard != "" {
+                self.performSegue(withIdentifier: "fromWalletToCardDetails", sender: self)
+            }
+            tableView.isUserInteractionEnabled = false
+            
+        }
+    }
+    
+    
+}
+
