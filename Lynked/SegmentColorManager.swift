ddb1c683ebd5a1bf2e2c6e884a34149d0610b574
mattmega4 <matthewhsingleton@Matthews-MacBook-Pro.local>
2017-06-15 10:26:54 -0700

    colors fixed on card, looking to fix segmented controls too

--- Lynked/SegmentColorManager.swift
+++ Lynked/SegmentColorManager.swift
@@ -12,7 +12,13 @@ class SegmentColorManager: NSObject {
     
     static let shared = SegmentColorManager()
     
-    private let colors = [["type" : 0, "color" : UIColor.red], ["type" : 0, "color" : UIColor.green], ["type" : 0, "color" : UIColor.blue], ["type" : 0, "color" : UIColor.black], ["type" : 1, "color" : UIColor.gray], ["type" : 1, "color" : UIColor.yellow]]
+    
+    private let colors = [["type" : 0, "color" : UIColor.init(red: 181.0/255.0, green: 49.0/255.0, blue: 59.0/255.0, alpha: 1.0)],
+                          ["type" : 0, "color" : UIColor.init(red: 0.0/255.0, green: 97.0/255.0, blue: 36.0/255.0, alpha: 1.0)],
+                          ["type" : 0, "color" : UIColor.init(red: 33.0/255.0, green: 91.0/255.0, blue: 158.0/255.0, alpha: 1.0)],
+                          ["type" : 0, "color" : UIColor.init(red: 41.0/255.0, green: 40.0/255.0, blue: 35.0/255.0, alpha: 1.0)],
+                          ["type" : 1, "color" : UIColor.init(red: 213.0/255.0, green: 212.0/255.0, blue: 219.0/255.0, alpha: 1.0)],
+                          ["type" : 1, "color" : UIColor.init(red: 197.0/255.0, green: 179.0/255.0, blue: 88.0/255.0, alpha: 1.0)]]
     
     func colorAtIndex(index: Int) -> UIColor {
         if index < colors.count {
