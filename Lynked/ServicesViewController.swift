7b1f6c0ad2b19cc5037c33a38505f1a649f21a8b
mattmega4 <matthewhsingleton@Matthews-MacBook-Pro.local>
2017-06-20 00:31:23 -0700

    Started to add Categories, on edit Service VC i added a 2nd UIPickerView, need to set that up. Redesigning/enaming CardDetailViewController to ServiceViewController. In it, Services can be viewed by category (new) or all at once. I need to list out all possible categories, leave one as Other. PIckerview on ServiceVC needs to be horizontal, (see notes)

---
+++ Lynked/ServicesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ServicesViewController.swift
+//  Lynked
+//
+//  Created by Matthew Howes Singleton on 6/20/17.
+//  Copyright © 2017 Matthew Singleton. All rights reserved.
+//
+
+import UIKit
+
+class ServicesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
