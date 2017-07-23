4edfaeedfb2c333af9fa21cecc3b50b47b729d09
mattmega4 <matthewhsingleton@matthews-macbook-pro.local>
2017-07-23 01:17:47 -0700

    Added Legal VC, submitted

---
+++ Lynked/LegalViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LegalViewController.swift
+//  Lynked
+//
+//  Created by Matthew Howes Singleton on 7/23/17.
+//  Copyright © 2017 Matthew Singleton. All rights reserved.
+//
+
+import UIKit
+
+class LegalViewController: UIViewController {
+
+    @IBOutlet weak var leftNavBarButton: UIBarButtonItem!
+    @IBOutlet weak var legalLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setNavBar()
+        title = "Legal"
+    }
+
+
+    @IBAction func leftNavBarButtonTapped(_ sender: UIBarButtonItem) {
+         navigationController?.popViewController(animated: true)
+    }
+    
+
+}
+
+
