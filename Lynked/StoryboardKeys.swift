92c90610be873128106c479dee34e1f963852a76
mattmega4 <matthewhsingleton@Matthews-MacBook-Pro.local>
2017-08-13 17:39:37 -0700

    pod update

---
+++ Lynked/StoryboardKeys.swift
@@ -0,0 +1,29 @@
+//
+//  StoryboardKeys.swift
+//  Lynked
+//
+//  Created by Matthew Howes Singleton on 8/10/17.
+//  Copyright © 2017 Matthew Singleton. All rights reserved.
+//
+
+import Foundation
+
+struct StoryboardKeys {
+  
+  static let entryViewControllerStoryboardID = "LoginVC"
+  static let addCardViewControllerStoryboardID = "AddCardVC"
+  static let splitStoryboardID = "splitVC"
+  static let walletViewControllerStoryboardID = "WalletVC"
+  static let servicesViewControllerStoryboardID = "serviceListVC"
+  static let serviceDetailViewControllerStoryboardID = "updateServiceVC"
+  static let editCardViewControllerStoryboardID = "EditCardVC"
+  static let acknowledgementsViewControllerStoryboardID = "ackVC"
+  static let profileViewControllerStoryboardID = "profileVC"
+  static let feedbackViewControllerStoryboardID = "feedbackVC"
+  static let legalViewControllerStoryboardID = "legalVC"
+  
+
+}
+
+
+
